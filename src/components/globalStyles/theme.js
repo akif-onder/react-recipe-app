d82cb2e70dba0250bf4457c8d9ea83302d4432e1
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-13 22:43:45 +0300

    navbar generated and styled

--- src/components/globalStyles/theme.js
+++ src/components/globalStyles/theme.js
@@ -1,6 +1,11 @@
 const theme = {
-    colors:{},
-    screens:{}
-}
+  colors: {
+    navbarBgColor: "#e1f1dd",
+    mainColor: "#00adb5",
+    ogoColor: "#393e46",
+    linkColor: "#02475e",
+  },
+  screens: {},
+};
 
 export default theme;
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,9 +1,22 @@
-
+import {Link} from 'react-router-dom';
+import Nav, {Logo, Menu, MenuLink} from './Navbar.style';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Nav justify="space-between" wrap="wrap">
+      <Logo to="/">
+      <i>{"<Clarusway/>"}</i>
+      <span>Recipe</span>
+      </Logo>
 
-export default Navbar
+      <Menu>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="login">Logout</MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
+
+export default Navbar;
