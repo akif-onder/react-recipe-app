3488647cffb261827346350600fb6f46e25a9ce7
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-14 20:07:31 +0300

    styles added

--- src/components/nav/Navbar.style.jsx
+++ src/components/nav/Navbar.style.jsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
-import Flex from '../globalStyles/Flex';
-import {Link} from "react-router-dom"
+import Flex from '../../components/globalStyles/Flex';
+import { Link } from 'react-router-dom';
 
 const Nav = styled(Flex)`
-padding: 1rem 2rem;
-background: ${({theme})=>theme.colors.navbarBgColor};
+  padding: 1rem 2rem;
+  background: ${({ theme }) => theme.colors.navbarBgColor};
 `;
 
 export const Logo = styled(Link)`
- padding: 1rem 0;
+  padding: 1rem 0;
   color: ${({ theme }) => theme.colors.logoColor};
   text-decoration: none;
   font-weight: 800;
@@ -20,24 +20,26 @@ export const Logo = styled(Link)`
 `;
 
 export const Hamburger = styled.div`
-display: none;
-cursor: pointer;
-color:${({theme})=>theme.colors.mainColors};
-@media (max-width: ${({theme})=>theme.screens.lg}){
-    display: block
-}
+  display: none;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.mainColor};
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    display: block;
+  }
 `;
+
 export const Menu = styled(Flex)`
-@media (max-width: ${({theme})=>theme.screens.lg}){
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
     flex-direction: column;
-    width:100%;
-    max-height:${({isOpen})=>isOpen ? '400px' : '0'};
+    width: 100%;
+    max-height: ${({ isOpen }) => (isOpen ? '400px' : '0')};
     overflow: hidden;
-    }
+    transition: max-height 0.3s ease-in;
+  }
 `;
 
 export const MenuLink = styled(Link)`
-    text-align: center;
+  text-align: center;
   padding: 1rem 2rem;
   cursor: pointer;
   text-decoration: none;
@@ -47,6 +49,9 @@ export const MenuLink = styled(Link)`
     color: ${({ theme }) => theme.colors.mainColor};
     font-weight: bold;
   }
-`
-
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    border-bottom: 1px solid ${({ theme }) => theme.colors.mainColor};
+    width: 100%;
+  }
+`;
 export default Nav;
